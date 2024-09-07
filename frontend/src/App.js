64e4952c1d5517b76208a99bc64dd9171819d646
Kal <info@kalacademy.org>
2024-09-07 11:04:57 -0700

    Initial setup of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import {Container} from 'react-bootstrap'
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to my shop!</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to my shop!</h1>
+        </Container>        
+      </main>
+      <Footer />
     </>
   );
 }
