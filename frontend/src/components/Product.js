95c4a3628390d0a06ff00e92d42676eb6c78d7d8
Kal <info@kalacademy.org>
2024-09-14 11:04:25 -0700

    Product screen and setup backend project

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import Rating from './Rating'
 
 const Product = ({product}) => {
   return (
     <Card className='my-5 p-3 rounded'>
-      <Card.Img src={product.image} variant='top' />
-
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </Link>
       <Card.Body>
-        <Card.Title as='div'>
-          <strong>{product.name}</strong>
-        </Card.Title>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
         <Card.Text as='div'>
           <Rating value={product.rating} text={`${product.numReviews} reviews`} />
         </Card.Text>
