e43b95a39c0ff426f1007a5b2af6528af8124b07
Kal <info@kalacademy.org>
2024-10-05 11:40:10 -0700

    Complete redux flow

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,36 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-import Product from '../components/Product'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {Col,Row} from 'react-bootstrap'
+import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const HomeScreen = () => {
 
-  const [products, setProducts] = useState([])
-
+  const dispatch = useDispatch()
   useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get('/api/products')
-      setProducts(data)
-    }
+    dispatch(listProducts())
+  }, [dispatch])
 
-    fetchProducts()
-  })
+  const productList = useSelector((state) => state.productList)
+  const {loading, products, error} = productList
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map(p => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={p} />
-          </Col>
-        ))}
-      </Row>
+      {
+        loading ? (<Loader />) :
+        error ? (<Message variant='danger'>{error}</Message>) :
+          (<Row>
+          {products.map(p => (
+            <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={p} />
+            </Col>
+          ))}
+        </Row>)
+      }
+      
     </>
   )
 }
