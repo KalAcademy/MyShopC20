d626077a87eb0c1b7a94a666c6cae733b94a55f6
Kal <info@kalacademy.org>
2024-11-02 10:51:51 -0700

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -18,5 +19,14 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
+
 app.use(errorHandler)
-app.listen(5000, console.log('Server is now running on port 5000'))
+const port = process.env.PORT
+app.listen(port, console.log(`Server is now running on port ${port}`))
